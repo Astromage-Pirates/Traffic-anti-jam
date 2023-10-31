b93e6a16d7b08623a0efb736069d4bb2b68bb049
Khang Dui <khangduyb4@gmail.com>
2023-10-31 17:47:57 +0700

    clamp camera movement and rotation TAJ-27

--- src/Traffic-anti-jam/Assets/ayo/CameraSystem.cs
+++ src/Traffic-anti-jam/Assets/ayo/CameraSystem.cs
@@ -1,32 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class CameraSystem : MonoBehaviour
 {
     private Vector3 direction;
-    private Vector2 inputValue;
-    private float speed = 10f;
+    private Vector2 directInputValue;
+    private float rotationValue;
+    private float moveSpeed = 15f;
+    private float rotateSoeed = 10f;
+    private float axisX = 20f;
+    private float axisZ = 20f;
+
+    [SerializeField]
+    private CinemachineVirtualCamera vCamera;
+    private CinemachineTransposer transposer;
+
+    private void Awake()
+    {
+        transposer = vCamera.GetCinemachineComponent<CinemachineTransposer>();
+    }
 
     private void FixedUpdate()
     {
-        transform.position += direction * speed * Time.deltaTime;
-
+        CameraMovement();
+        CameraRotation();
     }
 
-    // Update is called once per frame
-    public void Movement(InputAction.CallbackContext context)
+    public void CameraMovementDirection(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            inputValue = context.ReadValue<Vector2>();
-            direction = transform.forward * inputValue.y + transform.right * inputValue.x;
+            directInputValue = context.ReadValue<Vector2>();
+            direction =
+                transform.forward * directInputValue.y + transform.right * directInputValue.x;
         }
         else if (context.canceled)
         {
-            direction = Vector2.zero;
+            direction = Vector3.zero;
         }
     }
 
+    private void CameraMovement()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -axisX, axisX),
+            0,
+            Mathf.Clamp(transform.position.z, -axisZ + transposer.m_FollowOffset.z / 2, axisZ)
+        );
+
+        transform.Translate(
+            direction.x * moveSpeed * Time.deltaTime,
+            0,
+            direction.z * moveSpeed * Time.deltaTime
+        );
+    }
+
+    public void CameraRotateDirection(InputAction.CallbackContext context)
+    {
+        Debug.Log("aaa");
+        if (context.performed)
+        {
+            rotationValue = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            rotationValue = 0;
+        }
+    }
+
+    private void CameraRotation()
+    {
+        transform.eulerAngles += new Vector3(0, rotationValue * rotateSoeed * Time.deltaTime, 0);
+    }
 }
