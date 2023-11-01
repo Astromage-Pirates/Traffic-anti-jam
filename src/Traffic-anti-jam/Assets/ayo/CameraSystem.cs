cd8c3696e18ff8b431e0ba9210e3444c15112a3a
Khang Dui <khangduyb4@gmail.com>
2023-11-01 16:58:21 +0700

    zoom camera with FoV TAJ-27

--- src/Traffic-anti-jam/Assets/ayo/CameraSystem.cs
+++ src/Traffic-anti-jam/Assets/ayo/CameraSystem.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using Unity.IO.LowLevel.Unsafe;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,10 +12,15 @@ public class CameraSystem : MonoBehaviour
     private Vector3 direction;
     private Vector2 directInputValue;
     private float rotationValue;
+    private float targetFoV = 60f;
+    private float zoomStep = 5f;
+    private float zoomSpeed = 10f;
+    private float minFoV = 20f;
+    private float maxFoV = 80f;
     private float moveSpeed = 35f;
-    private float rotateSoeed = 30f;
     private float axisX = 20f;
     private float axisZ = 20f;
+    private float rotateSoeed = 30f;
 
     [SerializeField]
     private CinemachineVirtualCamera vCamera;
@@ -28,6 +35,7 @@ public class CameraSystem : MonoBehaviour
     {
         CameraMovement();
         CameraRotation();
+        CameraZoom();
     }
 
     public void CameraMovementDirection(InputAction.CallbackContext context)
@@ -69,5 +77,29 @@ public class CameraSystem : MonoBehaviour
     private void CameraRotation()
     {
         transform.Rotate(0.0f, rotationValue * rotateSoeed * Time.deltaTime, 0.0f, Space.World);
+    }
+
+    public void CameraZoomInputValue(InputAction.CallbackContext contest)
+    {
+        if (contest.performed)
+        {
+            targetFoV += contest.ReadValue<float>() * zoomStep * Time.deltaTime;
+            Debug.Log(targetFoV);
+        }
+        else if (contest.canceled)
+        {
+            targetFoV += 0.0f;
+        }
+
+        targetFoV = Mathf.Clamp(targetFoV, minFoV, maxFoV);
+    }
+
+    private void CameraZoom()
+    {
+        vCamera.m_Lens.FieldOfView = Mathf.Lerp(
+            vCamera.m_Lens.FieldOfView,
+            targetFoV,
+            Time.deltaTime * zoomSpeed
+        );
     }
 }
