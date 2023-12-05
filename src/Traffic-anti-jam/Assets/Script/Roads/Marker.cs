b89d0dbc3ed6951845b66b1d455d72902b09ef8c
CPea <cpea2506@gmail.com>
2023-12-06 01:46:31 +0700

    Setup new layout for `VehicleTestScene` TAJ-11

---
+++ src/Traffic-anti-jam/Assets/Script/Roads/Marker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    [SerializeField]
+    private List<Marker> adjacentMarkers;
+
+    /// <summary>
+    /// Whether this <see cref="Marker"/> is open for connection.
+    /// </summary>
+    [field: SerializeField]
+    public bool OpenForConnections { get; private set; }
+
+    /// <summary>
+    /// Get all of other adjacent <see cref="Marker"/>s positions.
+    /// </summary>
+    public List<Vector3> GetAdjacentPositions =>
+        new List<Vector3>(adjacentMarkers.Select(x => x.transform.position).ToList());
+
+    private void OnDrawGizmos()
+    {
+        if (Selection.activeObject == gameObject)
+        {
+            Gizmos.color = Color.red;
+
+            if (adjacentMarkers != null && !adjacentMarkers.IsEmpty())
+            {
+                foreach (var item in adjacentMarkers)
+                {
+                    Gizmos.DrawLine(transform.position, item.transform.position);
+                }
+            }
+
+            Gizmos.color = Color.white;
+        }
+    }
+}
