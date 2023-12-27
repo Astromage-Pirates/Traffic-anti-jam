1627821a9bedf0fbb94b45ab1be298eef69a5830
Khang Duy Nguyen <khangduyb4@gmail.com>
2023-12-27 22:34:10 +0700

    add speed property to MinSpeed sign TAJ-9

--- src/Traffic-anti-jam/Assets/Script/Traffic Tool/MinSpeedSign.cs
+++ src/Traffic-anti-jam/Assets/Script/Traffic Tool/MinSpeedSign.cs
@@ -2,4 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinSpeedSign : TrafficSign { }
+public class MinSpeedSign : TrafficSign
+{
+    [SerializeField]
+    private int minSpeed;
+    public int MinSpeed => minSpeed;
+}
