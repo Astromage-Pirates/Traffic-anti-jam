53d0b960d78ac2cff44a195633c2f83139799a69
CPea <cpea2506@gmail.com>
2023-12-29 00:54:19 +0700

    Add functionality for game over canvas button

--- src/Traffic-anti-jam/Assets/Script/UI/menu/PauseMenu.cs
+++ src/Traffic-anti-jam/Assets/Script/UI/menu/PauseMenu.cs
@@ -12,12 +12,6 @@ public class PauseMenu : MonoBehaviour
     [SerializeField]
     private Button btn_Close;
 
-    [SerializeField]
-    private Button btn_Exit;
-
-    [SerializeField]
-    private Button btn_Restart;
-
     [Header("Main Menu")]
     [SerializeField]
     private GameObject grp_MainMenu;
@@ -39,16 +33,12 @@ public class PauseMenu : MonoBehaviour
     private void OnEnable()
     {
         btn_Settings.onClick.AddListener(OnBtnSettingsClicked);
-        btn_Restart.onClick.AddListener(OnBtnRestartClicked);
-        btn_Exit.onClick.AddListener(OnBtnExitClicked);
         btn_Close.onClick.AddListener(OnBtnCloseClicked);
     }
 
     private void OnDisable()
     {
         btn_Settings.onClick.RemoveListener(OnBtnSettingsClicked);
-        btn_Restart.onClick.RemoveListener(OnBtnRestartClicked);
-        btn_Exit.onClick.RemoveListener(OnBtnExitClicked);
         btn_Close.onClick.RemoveListener(OnBtnCloseClicked);
     }
 
@@ -60,16 +50,5 @@ public class PauseMenu : MonoBehaviour
     private void OnBtnCloseClicked()
     {
         menuManager.ExitMenu(grp_MainMenu);
-    }
-
-    private void OnBtnRestartClicked()
-    {
-        var activeScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(activeScene.buildIndex);
-    }
-
-    private void OnBtnExitClicked()
-    {
-        SceneManager.LoadScene(0);
     }
 }
