7bd4ff729c3fa34f89e5716589c81597f8984a32
CPea <cpea2506@gmail.com>
2023-12-28 22:43:20 +0700

    Rename file `AudioMixerExtensions.cs` to `SoundGroup.cs`

--- src/Traffic-anti-jam/Assets/Script/Audios/SoundGroup.cs
+++ src/Traffic-anti-jam/Assets/Script/Audios/SoundGroup.cs
@@ -1,42 +1,34 @@
-using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Audio;
-using static SoundGroup;
 
 /// <summary>
 /// Enumeration of the mixer sound groups.
 /// </summary>
 public enum SoundGroup
 {
-    Master,
-    Background,
-    Ambient,
-    Sfx,
+    MasterVolume,
+    BackgroundVolume,
+    AmbientVolume,
+    SfxVolume,
 }
 
 /// <summary>
 /// Extension class that add additional useful stuffs for <see cref="AudioMixer"/>.
 /// </summary>
-public static class AudioMixerExtensions
+public static class SoundGroupExtensions
 {
+    private const float MinSliderValue = 0.0001f;
+    private const float MaxSliderValue = 1f;
     private const float AmplitudeFactor = 20f;
-
-    /// <summary>
-    /// Helper property to get exposed parameter name based on <see cref="SoundGroup"/>
-    /// </summary>
-    public readonly static Dictionary<SoundGroup, string> MixerExposedParams = new Dictionary<
-        SoundGroup,
-        string
-    >
-    {
-        { Background, "BackgroundVolume" },
-        { Sfx, "SfxVolume" },
-        { Ambient, "AmbientVolume" },
-        { Master, "MasterVolume" },
-    };
 
     public static float ConvertToMixerValue(this float value)
     {
+        Assert.IsTrue(
+            value >= MinSliderValue & value <= MaxSliderValue,
+            $"Value must be in range ({MinSliderValue}, {MaxSliderValue})"
+        );
+
         return Mathf.Log10(value) * AmplitudeFactor;
     }
 
@@ -47,7 +39,7 @@ public static class AudioMixerExtensions
     /// <param name="audioMixer">The <see cref="AudioMixer"/> that contains the provided <see cref="SoundGroup"/>.</param>
     public static void SetVolume(this SoundGroup group, AudioMixer audioMixer)
     {
-        var key = MixerExposedParams[group];
+        var key = group.ToString();
         var volume = PlayerPrefs.GetFloat(key, 1f);
 
         audioMixer.SetFloat(key, volume.ConvertToMixerValue());
