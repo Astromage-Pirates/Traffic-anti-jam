099e819abc8eed28c77ddb5e81174bc0f5baa02f
Khang Duy Nguyen <khangduyb4@gmail.com>
2023-12-29 18:14:55 +0700

    traffic sign control path TAJ-9

--- src/Traffic-anti-jam/Assets/Script/Traffic Tool/TrafficSign.cs
+++ src/Traffic-anti-jam/Assets/Script/Traffic Tool/TrafficSign.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class TrafficSign : TrafficTool
 {
+    private TrafficSignSnapPoint trafficSignSnapPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<TrafficSignSnapPoint>(out TrafficSignSnapPoint component))
+        {
+            trafficSignSnapPoint = component;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        trafficSignSnapPoint.UnDoPath();
+    }
 }
