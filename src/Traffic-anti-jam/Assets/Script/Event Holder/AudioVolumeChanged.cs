f41abc579466dc6a698b8bc1ae9fb584e8cf8e8a
CPea <cpea2506@gmail.com>
2023-12-29 09:24:49 +0700

    Send `SoundGroup` each time a volume slider change value

--- src/Traffic-anti-jam/Assets/Script/Event Holder/AudioVolumeChanged.cs
+++ src/Traffic-anti-jam/Assets/Script/Event Holder/AudioVolumeChanged.cs
@@ -3,4 +3,7 @@ using AstroPirate.DesignPatterns;
 /// <summary>
 /// Event for when the <see cref="Audio"/> volume changed.
 /// </summary>
-public class AudioVolumeChanged : EventContext { }
+public class AudioVolumeChanged : EventContext
+{
+    public SoundGroup SoundGroup;
+}
